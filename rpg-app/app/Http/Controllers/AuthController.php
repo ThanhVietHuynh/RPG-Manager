1c4ded3e91203962646c49177cea769a0ade34b5
ThanhVietHuynh <huynhthanhviet13006@gmail.com>
2022-11-28 14:54:22 +0100

    plus d'erreur

--- rpg-app/app/Http/Controllers/AuthController.php
+++ rpg-app/app/Http/Controllers/AuthController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Session;
+use Illuminate\Support\Facades\Session;
 use App\Models\User;
-use Hash;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -45,8 +45,7 @@ class AuthController extends Controller
    
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('dashboard')
-                        ->withSuccess('You have Successfully loggedin');
+            return redirect()->intended('dashboard')->withSuccess('You have Successfully loggedin');
         }
   
         return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
