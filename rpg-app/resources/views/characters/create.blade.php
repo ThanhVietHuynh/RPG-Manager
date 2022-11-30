88c90b45f93999a29e5d7941bbdf09d7cbcde168
ThanhVietHuynh <huynhthanhviet13006@gmail.com>
2022-11-30 15:17:56 +0100

    formulaire character

--- rpg-app/resources/views/characters/create.blade.php
+++ rpg-app/resources/views/characters/create.blade.php
@@ -8,55 +8,96 @@
     <form action="{{route('characters.store')}}" method="POST">
         @csrf
 
-        <div>
-            <label for="character_name">Nom du personnage :</label>
-            <input type="text" id="character_name" 
-            placeholder="Entrez un nom" name="character_name"/>
-        </div>
-
-        <div>
-
-            <label for="character_description">Description du personnage:</label>
-            <input type="text" id="character_description" 
-            placeholder="Entrez une description" name="character_description"/>
-
-        </div>
-
-        <div>
-            <label for="speciality">Type:</label>
-            <select name="speciality" id="speciality">
-                <option value="Guerrier">Guerrier</option>
-                <option value="Mage">Mage</option>
-                <option value="Druide">Druide</option>
-                <option value="Assassin">Assassin</option>
-                <option value="Berseker">Berseker</option>
-                <option value="Archer">Archer</option>
-            </select>
-        </div>
-
-        <div>
-            <label for="mag">Magie :</label>
-            <input type="number" readonly value={{$mag}} class="reandonly" name="mag"/>
-        </div>
-        <div>
-            <label for="for">Force :</label>
-            <input type="number"readonly value={{$for}} class="reandonly" id="for" name="for"/>
-        </div>
-        <div>
-            <label for="agi">Agilité :</label>
-            <input type="number" readonly value={{$agi}} class="reandonly" id="agi" name="agi"/>
-        </div>
-        <div>
-            <label for="int">Intelligence :</label>
-            <input type="number" readonly value={{$int}} class="reandonly" id="int" name="int"/>
-        </div>
-        <div>
-            <label for="pv">PV :</label>
-            <input type="number" readonly value={{$pv}} class="reandonly" id="pv" name="pv"/>
-        </div>
-        <button type="submit">Créer mon personnage</button>
-        
-    </form>
-    <button onclick="myFunction()">Changer de statistique</button>
+        <form action="{{route('characters.store')}}" method="POST">
+            @csrf
     
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                    <div class="card-header">Ajouter un personnage</div>
+                    <div class="card-body">
+                        <form action="{{route('characters.store')}}" method="POST">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <input type="text" name="character_name" class="form-control" placeholder="Nom du personnage"/>
+                                @if($errors->has('character_name'))
+                                    <span class="text-danger">{{ $errors->first('character_name') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="form-floating">
+                                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" name="character_description"></textarea>
+                                    <label for="floatingTextarea">Comments</label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-3">
+                                <div>
+                                    <select class="form-select" aria-label="Default select example" name="speciality" id="speciality">
+                                        <option selected>Spéciality</option>
+                                        <option value="Guerrier">Guerrier</option>
+                                        <option value="Mage">Mage</option>
+                                        <option value="Druide">Druide</option>
+                                        <option value="Assassin">Assassin</option>
+                                        <option value="Berseker">Berseker</option>
+                                        <option value="Archer">Archer</option>
+                                      </select>
+                                </div>
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="form-floating mb-3">
+                                    <input type="number" class="form-control" id="mag" name="mag" readonly value={{$mag}}>
+                                    <label for="mag">Magie</label>
+                                </div>
+                                @if($errors->has('mag'))
+                                    <span class="text-danger">{{ $errors->first('mag') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="form-floating mb-3">
+                                    <input type="number" class="form-control" id="for" name="for" readonly value={{$for}}>
+                                    <label for="for">Force</label>
+                                </div>
+                                @if($errors->has('mag'))
+                                    <span class="text-danger">{{ $errors->first('for') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="form-floating mb-3">
+                                    <input type="number" class="form-control" id="agi" name="agi" readonly value={{$agi}}>
+                                    <label for="agi">Agilité</label>
+                                </div>
+                                @if($errors->has('mag'))
+                                    <span class="text-danger">{{ $errors->first('agi') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="form-floating mb-3">
+                                    <input type="number" class="form-control" id="int" name="int" readonly value={{$int}}>
+                                    <label for="int">Intelligence</label>
+                                </div>
+                                @if($errors->has('mag'))
+                                    <span class="text-danger">{{ $errors->first('int') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="form-floating mb-3">
+                                    <input type="number" class="form-control" id="floatingInput" name="pv" readonly value={{$pv}}>
+                                    <label for="floatingInput">PV</label>
+                                </div>
+                                @if($errors->has('mag'))
+                                    <span class="text-danger">{{ $errors->first('pv') }}</span>
+                                @endif
+                            </div>
+                                <button type="submit" class="btn btn-dark btn-block">Valider Personnage</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </form>
+        <button onclick="myFunction()">Changer de statistique</button>
+
+    </form>
+
 @endsection
